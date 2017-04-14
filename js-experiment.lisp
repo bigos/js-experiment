02d22232a3d29ede637c628af7379565b2094d04
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-14 15:56:14 +0100

    Written camelize function

--- js-experiment.lisp
+++ js-experiment.lisp
@@ -1,2 +1,20 @@
-(defun main ()
-  (format t "In JS experiment~%"))
+;;; js experiment
+
+(defun camelize (str &optional (acc ""))
+  (cond
+    ((eq 0 (length str))
+     acc)
+    ((equal str "-")
+     (error "String can not end with -"))
+    ((and (>=    (length str) 2)
+          (equal (subseq str 0 2) "--"))
+     (error "String can not contain --"))
+    (T
+     (let*
+         ((dash (equal "-" (subseq str 0 1)))
+          (first-start (if dash 1 0))
+          (first-end   (if dash 2 1))
+          (string-op   (if dash 'string-upcase 'string-downcase)))
+       (camelize (subseq str first-end)
+                 (concatenate 'string acc
+                              (funcall string-op (subseq str first-start first-end))))))))
