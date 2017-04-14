4399b98cca9f518804ec90a28e157e3c1557efe8
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-14 17:09:26 +0100

    attempt to write a converter

--- js-experiment.lisp
+++ js-experiment.lisp
@@ -1,20 +1,42 @@
 ;;; js experiment
 
+(declaim (optimize (debug 3)))
+
 (defun camelize (str &optional (acc ""))
+  "Helper upper casing letters preceded by '-' and down casing everything else."
   (cond
     ((eq 0 (length str))
      acc)
     ((equal str "-")
      (error "String can not end with -"))
-    ((and (>=    (length str) 2)
+    ((and (>= (length str) 2)
           (equal (subseq str 0 2) "--"))
      (error "String can not contain --"))
     (T
      (let*
-         ((dash (equal "-" (subseq str 0 1)))
-          (first-start (if dash 1 0))
-          (first-end   (if dash 2 1))
-          (string-op   (if dash 'string-upcase 'string-downcase)))
-       (camelize (subseq str first-end)
+         ((letter (not (equal "-" (subseq str 0 1))))
+          (letter-start (if letter 0 1))
+          (letter-end   (if letter 1 2))
+          (string-op    (if letter 'string-downcase 'string-upcase)))
+       (camelize (subseq str letter-end)
                  (concatenate 'string acc
-                              (funcall string-op (subseq str first-start first-end))))))))
+                              (funcall string-op
+                                       (subseq str letter-start letter-end))))))))
+
+(defun symbol-to-js (symbol)
+  "Converts SYMBOL to lowercase or camelCase string"
+  (camelize (symbol-name symbol)))
+
+;;; not working yet
+;;; (fun-to-js '(alert "hello"))
+;;; attempt to convert sexp to JS function(arguments)
+(defun fun-to-js (sexp)
+  (if (consp sexp)
+      (if (symbolp (car sexp))
+          (format nil "~a(~a)" (symbol-to-js (car sexp))
+                  (fun-to-js (cdr sexp)))
+          (format nil "~A" (car sexp)))
+      (format nil "~a" (if (symbolp sexp)
+                           (symbol-to-js sexp)
+                           sexp
+                           ))))
